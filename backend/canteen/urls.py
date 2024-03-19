c3cac16491404f6b60ea5ecfff26c47aa5aed668
Aaron <HMQEII>
2024-03-19 23:37:15 +0530

    redirection links added

--- backend/canteen/urls.py
+++ backend/canteen/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.views.generic.base import RedirectView
 from . import views
 
+
 urlpatterns = [
+    path('', RedirectView.as_view(url='/Login/')),
     path('Home/', views.home, name='home'),
     path('Login/', views.login, name='login'),
     path('Home/Veg/', views.Veg, name='veg'),
